9366ee735266e6bb509e4c00ae1b770d5906783b
yuehuizhang@faceunity.com <yuehuizhang@faceunity.com>
2018-12-17 14:58:41 +0800

    初始化项目结构

---
+++ src/routers/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { BrowserRouter, Link, Route } from "react-router-dom";
+
+const routes = (
+    <BrowserRouter>
+        <div>
+            <Route path="/"></Route>
+        </div>
+    </BrowserRouter>
+)
